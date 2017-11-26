179e21c9637831d89a768138327f78cbb3224825
Long Nguyen <nguyen_van_long@humancrest.co.jp>
2017-11-27 03:26:07 +0700

    Update backend with Authentication

---
+++ src/main/java/com/example/itblog/controller/DebugController.java
@@ -0,0 +1,16 @@
+package com.example.itblog.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DebugController {
+
+    @RequestMapping(value = "/debug", method = RequestMethod.GET)
+    @PreAuthorize("USER")
+    public String home() {
+        return "YOU HAVE ACCESSED TO DEBUG PAGE";
+    }
+}
